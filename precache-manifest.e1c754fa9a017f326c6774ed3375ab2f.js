c864b3fe08b7869168c9bb2af47121368a218249
Elena Nub <niallaris@mail.ru>
2025-06-02 22:55:35 +0300

    Updates

---
+++ precache-manifest.e1c754fa9a017f326c6774ed3375ab2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad803cc5db4a13b7bf0eb82edac078b8",
+    "url": "/vk-ptsd-test/index.html"
+  },
+  {
+    "revision": "a06f322751ab1e60ef7f",
+    "url": "/vk-ptsd-test/static/css/2.ad53036d.chunk.css"
+  },
+  {
+    "revision": "ac2d48e601301920bd3d",
+    "url": "/vk-ptsd-test/static/css/main.6036b6cb.chunk.css"
+  },
+  {
+    "revision": "a06f322751ab1e60ef7f",
+    "url": "/vk-ptsd-test/static/js/2.2a38964d.chunk.js"
+  },
+  {
+    "revision": "ac2d48e601301920bd3d",
+    "url": "/vk-ptsd-test/static/js/main.1595f4e4.chunk.js"
+  },
+  {
+    "revision": "2ea52e9c525ba97dc655",
+    "url": "/vk-ptsd-test/static/js/runtime~main.5a145df9.js"
+  },
+  {
+    "revision": "b5e9bf9655529ce0817d1fa4bf6a91d4",
+    "url": "/vk-ptsd-test/static/media/555.b5e9bf96.png"
+  }
+]);
